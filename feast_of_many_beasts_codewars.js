9a7505806bfcdb415c053d7bc8b052499da497ef
Crew Spence <csspence123@gmail.com>
2020-06-30 06:40:43 -0500

    Wrote working solution.

--- feast_of_many_beasts_codewars.js
+++ feast_of_many_beasts_codewars.js
@@ -6,6 +6,6 @@ Write a function feast that takes the animal's name and dish as arguments and re
 Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
 */
 
-function feast(beast, dish) {
-//your function here
+const feast = (beast, dish) => {
+  return beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1] ? true : false;
 }
